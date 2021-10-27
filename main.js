1e39ae12cf4588f784f6ce5100ca542b11d2f3cf
Amulya958 <85683096+Amulya958@users.noreply.github.com>
2021-10-27 18:10:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+Webcam.set 
+({
+width:360 ,
+height:250 ,
+image_format:'png' ,
+png_quality:99
+});
+camera= document.getElementById("camera") ;
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+
+Webcam.snap(function(data_uri)
+{
+    document.getElementById("result").innerHTML = '<img id="selfie_img" src="'+data_uri+'">';
+});
+}
+
+console.log('ml5 vesion :' , ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Sm1iSTYUJ/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+}
+
